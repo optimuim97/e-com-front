346c7e01ee3c189ca4ff99707d86cd9928c47cae
sidik.ouattara <sidik.ouattara@groupebatimat.com>
2026-05-19 09:28:31 +0000

    Add WhatsAppButton component and integrate it into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <RouterView />
+  <WhatsAppButton v-if="!isAdminRoute" />
 </template>
 
 <script setup>
-import { RouterView } from 'vue-router';
+import { computed } from 'vue';
+import { RouterView, useRoute } from 'vue-router';
+import WhatsAppButton from '@/components/shop/WhatsAppButton.vue';
+
+const route = useRoute();
+const isAdminRoute = computed(() => route.path.startsWith('/admin'));
 </script>
